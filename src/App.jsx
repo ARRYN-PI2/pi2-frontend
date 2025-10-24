fe17b2599686894ff1022e4db039f7a9ed1fcdd0
esedesofiaaa <sofiasilva101@gmail.com>
2025-10-23 21:49:38 -0500

    Fix import path for Products component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./pages/Styles/global.css";
 import Home from "./pages/Home/Home";
 import HomeLogin from './pages/HomeLogin/HomeLogin';
 import Login from "./pages/Login/Login";
-import Products from "./pages/Products/products";
+import Products from "../pages/Products/Products";
 import { Routes, Route } from "react-router-dom";
 
 export default function App() {
