c6fac913d727228bef6c660565b110f477765dea
Ncrod <rodriguezacevedonicolas@gmail.com>
2025-10-27 10:45:58 -0500

    config vite

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,30 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
+    // 👇 clave para que funcionen las imágenes en Pages (subcarpeta /<repo>/)
+    base: "/pi2-frontend/",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: "http://localhost:8000",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+    build: {
+      outDir: "dist",
+    },
+  };
 });
